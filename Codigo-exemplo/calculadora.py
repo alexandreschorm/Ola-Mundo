613a5f83711d03c9e16bb2f1d63caa987c0c94d9
Alexandre Schorm Barros <aleschormbarrosp321@gmail.com>
2026-07-17 00:16:32 -0400

    primeira melhoria da calculadora
    Adicionei subtração, multiplicação e divisão. Ainda também, tornei a mecânica da calculadora uma função, para testar novos elementos no GitHub

--- Codigo-exemplo/calculadora.py
+++ Codigo-exemplo/calculadora.py
@@ -1,3 +1,18 @@
-a = int(input("Digite um número: "))
-b = int(input("Digite outro número: "))
-print(f"A soma de {a} mais {b} é: {a+b}")
+def calcula_resultado(x, num1, num2):
+    if x == "+":
+        resultado = num1 + num2
+    elif x == "-":
+        resultado = num1 - num2
+    elif x == "*":
+        resultado = num1 * num2
+    else:
+        resultado = num1 / num2
+    return resultado
+
+x = input("Escolha a operação que deseja realizar (+, -, * ou /): ")
+a = int(input("Digite o primeiro número: "))
+b = int(input("Digite o segundo número: "))
+
+resultado = calcula_resultado(x, a, b)
+
+print(f"O resultado de {a} e {b} é: {resultado}")
